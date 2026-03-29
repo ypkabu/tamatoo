c617a1a0c0775c7c9ec6511220ede9a8a9e9670a
ypkabu <zhushihuisheyp028@gmail.com>
2026-03-29 17:59:14 +0900

    a

---
+++ Plugins/UltraleapTracking_ue5_4-5.0.1/Source/UltraleapTrackingCore/Private/LeapBlueprintFunctionLibrary.cpp
@@ -0,0 +1,231 @@
+/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+
+#include "LeapBlueprintFunctionLibrary.h"
+
+#include "IUltraleapTrackingPlugin.h"
+#include "Misc/ConfigCacheIni.h"
+#include "LeapSubsystem.h"
+
+#if PLATFORM_ANDROID
+#include "Android/AndroidApplication.h"
+#include "Android/AndroidJNI.h"
+#include <android/log.h>
+#endif
+
+FRotator ULeapBlueprintFunctionLibrary::DebugRotator;
+ULeapBlueprintFunctionLibrary::ULeapBlueprintFunctionLibrary(const class FObjectInitializer& Initializer) : Super(Initializer)
+{
+}
+
+void ULeapBlueprintFunctionLibrary::SetLeapMode(
+	ELeapMode InMode, const TArray<FString>& DeviceSerials, ELeapTrackingFidelity InTrackingFidelity)
+{
+	// if empty, default to single device logic
+	if (!DeviceSerials.Num())
+	{
+		FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions("");
+
+		Options.Mode = InMode;
+		Options.TrackingFidelity = InTrackingFidelity;
+
+		SetLeapOptions(Options, DeviceSerials);
+	}
+	else
+	{
+		for (auto DeviceSerial : DeviceSerials)
+		{
+			FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions(DeviceSerial);
+
+			Options.Mode = InMode;
+			Options.TrackingFidelity = InTrackingFidelity;
+
+			TArray<FString> SingleDevice;
+			SingleDevice.Add(DeviceSerial);
+			SetLeapOptions(Options, SingleDevice);
+		}
+	}
+}
+
+void ULeapBlueprintFunctionLibrary::SetLeapOptions(const FLeapOptions& InOptions, const TArray<FString>& DeviceSerials)
+{
+	if (ULeapSubsystem* LeapSubsystem = ULeapSubsystem::Get())
+	{
+		LeapSubsystem->SetUseOpenXR(InOptions.bUseOpenXRAsSource);
+	}
+	IUltraleapTrackingPlugin::Get().SetOptions(InOptions, DeviceSerials);
+}
+
+void ULeapBlueprintFunctionLibrary::GetLeapOptions(FLeapOptions& OutOptions, const FString& DeviceSerial)
+{
+	OutOptions = IUltraleapTrackingPlugin::Get().GetOptions(DeviceSerial);
+}
+
+void ULeapBlueprintFunctionLibrary::GetLeapStats(FLeapStats& OutStats, const FString& DeviceSerial)
+{
+	OutStats = IUltraleapTrackingPlugin::Get().GetLeapStats(DeviceSerial);
+}
+
+void ULeapBlueprintFunctionLibrary::SetLeapPolicy(ELeapPolicyFlag Flag, bool Enable, const TArray<FString>& DeviceSerials)
+{
+	IUltraleapTrackingPlugin::Get().SetLeapPolicy(Flag, Enable, DeviceSerials);
+}
+void ULeapBlueprintFunctionLibrary::GetAttachedLeapDevices(TArray<FString>& Devices)
+{
+	IUltraleapTrackingPlugin::Get().GetAttachedDevices(Devices);
+}
+FString ULeapBlueprintFunctionLibrary::GetAppVersion()
+{
+	FString AppVersion;
+	if (GConfig)
+	{
+		GConfig->GetString(TEXT("/Script/EngineSettings.GeneralProjectSettings"), TEXT("ProjectVersion"), AppVersion, GGameIni);
+	}
+	return AppVersion;
+}
+// Debug Functions
+void ULeapBlueprintFunctionLibrary::ShutdownLeap()
+{
+	IUltraleapTrackingPlugin::Get().ShutdownLeap();
+}
+void ULeapBlueprintFunctionLibrary::SetDebugRotation(const FRotator& Rotator)
+{
+	DebugRotator = Rotator;
+}
+float ULeapBlueprintFunctionLibrary::AngleBetweenVectors(const FVector& A, const FVector& B)
+{
+	float AngleCosine = FVector::DotProduct(A, B) / (A.Size() * B.Size());
+	float AngleRadians = FMath::Acos(AngleCosine);
+	return FMath::RadiansToDegrees(AngleRadians);
+}
+
+void ULeapBlueprintFunctionLibrary::SetLeapDeviceHints(const TArray<FString>& DeviceSerials, const TArray<FString>& Hints)
+{
+	// if empty, default to single device logic
+	if (!DeviceSerials.Num())
+	{
+		FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions("");
+		if (Options.LeapHints != Hints)
+		{
+			Options.LeapHints = Hints;
+			SetLeapOptions(Options, DeviceSerials);
+		}
+	}
+	else
+	{
+		for (FString DeviceSerial : DeviceSerials)
+		{
+			FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions(DeviceSerial);
+			if (Options.LeapHints != Hints)
+			{
+				Options.LeapHints = Hints;
+				TArray<FString> SingleDevice;
+				SingleDevice.Add(DeviceSerial);
+				SetLeapOptions(Options, SingleDevice);
+			}
+		}
+	}
+}
+
+void ULeapBlueprintFunctionLibrary::AddLeapDeviceHint(const TArray<FString>& DeviceSerials, const FString& Hint)
+{
+	if (!DeviceSerials.Num())
+	{
+		FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions("");
+		if (!Options.LeapHints.Contains(Hint))
+		{
+			Options.LeapHints.Add(Hint);
+			SetLeapOptions(Options, DeviceSerials);
+		}
+	}
+	else
+	{
+		for (FString DeviceSerial : DeviceSerials)
+		{
+			FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions(DeviceSerial);
+			if (!Options.LeapHints.Contains(Hint))
+			{
+				Options.LeapHints.Add(Hint);
+				TArray<FString> SingleDevice;
+				SingleDevice.Add(DeviceSerial);
+				SetLeapOptions(Options, SingleDevice);
+			}
+		}
+	}
+}
+
+void ULeapBlueprintFunctionLibrary::RemoveLeapDeviceHint(const TArray<FString>& DeviceSerials, const FString& Hint)
+{
+	
+	if (!DeviceSerials.Num())
+	{
+		FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions("");
+		if (Options.LeapHints.Contains(Hint))
+		{
+			Options.LeapHints.Remove(Hint);
+			SetLeapOptions(Options, DeviceSerials);
+		}
+	}
+	else
+	{
+		for (FString DeviceSerial : DeviceSerials)
+		{
+			FLeapOptions Options = IUltraleapTrackingPlugin::Get().GetOptions(DeviceSerial);
+			if (Options.LeapHints.Contains(Hint))
+			{
+				Options.LeapHints.Remove(Hint);
+				TArray<FString> SingleDevice;
+				SingleDevice.Add(DeviceSerial);
+				SetLeapOptions(Options, SingleDevice);
+			}
+		}
+	}
+}
+
+void ULeapBlueprintFunctionLibrary::BindTrackingServiceAndroid()
+{
+#if PLATFORM_ANDROID
+	// Binding the tracking service
+	if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())
+	{
+		static jmethodID Method =
+			FJavaWrapper::FindMethod(Env, FJavaWrapper::GameActivityClassID, "AndroidThunkJava_Bind", "()V", false);
+		if (Method)
+		{
+			UE_LOG(UltraleapTrackingLog, Log, TEXT("UltraleapTracking: calling AndroidThunkJava_Bind"));
+			FJavaWrapper::CallVoidMethod(Env, FJavaWrapper::GameActivityThis, Method);
+		}
+		else
+		{
+			UE_LOG(UltraleapTrackingLog, Error, TEXT("UltraleapTracking: could not call AndroidThunkJava_Bind invalid Method"));
+		}
+	}
+#endif
+}
+
+void ULeapBlueprintFunctionLibrary::UnbindTrackingServiceAndroid()
+{
+#if PLATFORM_ANDROID
+	//  Unbind the tracking service
+	if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())
+	{
+		static jmethodID Method =
+			FJavaWrapper::FindMethod(Env, FJavaWrapper::GameActivityClassID, "AndroidThunkJava_Unbind", "()V", false);
+		if (Method)
+		{
+			UE_LOG(UltraleapTrackingLog, Log, TEXT("UltraleapTracking: calling AndroidThunkJava_Unbind"));
+			FJavaWrapper::CallVoidMethod(Env, FJavaWrapper::GameActivityThis, Method);
+		}
+		else
+		{
+			UE_LOG(UltraleapTrackingLog, Error, TEXT("UltraleapTracking: could not call AndroidThunkJava_Unbind invalid Method"));
+		}
+	}
+#endif
+}
---
+++ Plugins/UltraleapTracking_ue5_4-5.0.1/Source/UltraleapTrackingCore/UltraleapTracking.Build.cs
@@ -0,0 +1,258 @@
+/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+using System.IO;
+ 
+namespace UnrealBuildTool.Rules
+{
+	public class UltraleapTracking : ModuleRules
+	{
+		private bool IsEnginePlugin()
+		{
+			return Path.GetFullPath(ModuleDirectory).EndsWith("Engine\\Plugins\\Runtime\\UltraleapTracking\\Source\\UltraleapTracking");
+		}
+
+		private string ModulePath
+		{
+			get { return ModuleDirectory; }
+		}
+
+		private string ThirdPartyPath
+		{
+			get
+			{
+				if (IsEnginePlugin())
+				{
+					return Path.GetFullPath(Path.Combine(EngineDirectory, "Source/ThirdParty"));
+				}
+				else
+				{
+					return Path.GetFullPath(Path.Combine(ModulePath, "../ThirdParty/"));
+				}
+			}
+		}
+
+		private string BinariesPath
+		{
+			get
+			{
+				if (IsEnginePlugin())
+				{
+					return Path.GetFullPath(Path.Combine(EngineDirectory, "Binaries/ThirdParty/UltraleapTracking"));
+				}
+				else
+				{ 
+					return Path.GetFullPath(Path.Combine(ModulePath, "../../Binaries/"));
+				}
+			}
+		}
+
+		private string LibraryPath
+		{
+			get
+			{
+				if (IsEnginePlugin())
+				{
+					return Path.GetFullPath(Path.Combine(ThirdPartyPath, "Leap/Lib"));
+				}
+				else
+				{
+					return Path.GetFullPath(Path.Combine(ThirdPartyPath, "LeapSDK/Lib"));
+				}
+			}
+		}
+
+		private string IncludePath
+		{
+			get
+			{
+				if (IsEnginePlugin())
+				{
+					return Path.GetFullPath(Path.Combine(ThirdPartyPath, "Leap/Include"));
+				}
+				else
+				{
+					return Path.GetFullPath(Path.Combine(ThirdPartyPath, "LeapSDK/Include"));
+				}
+			}
+		}
+
+        private void Setlib(string platformStr, string libStr)
+        {
+            PublicAdditionalLibraries.Add(Path.Combine(BinariesPath, platformStr, libStr));
+            PublicDelayLoadDLLs.Add(Path.Combine(BinariesPath, platformStr, libStr));
+            RuntimeDependencies.Add(Path.Combine(BinariesPath, platformStr, libStr));
+        }
+
+        public UltraleapTracking(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+			//OptimizeCode = CodeOptimization.Never;
+			PublicIncludePaths.AddRange(
+				new string[] {
+                   
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					IncludePath,
+					// ... add other private include paths required here ...
+				}
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+                    "ApplicationCore",
+                    "Engine",
+					"Core",
+					"CoreUObject",
+					"InputCore",
+					"InputDevice",
+					"Slate",
+					"SlateCore",
+					"HeadMountedDisplay",
+					"RHI",
+					"RenderCore",
+					"Projects",
+					"LiveLinkInterface",
+					"LiveLinkMessageBusFramework",
+					"BodyState",
+					"PhysicsCore",
+					"UMG",
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+            if (Target.Version.MajorVersion >= 5 && Target.Version.MinorVersion >= 3)
+            {
+                PrivateDependencyModuleNames.AddRange(new string[] { "XRBase" });
+            }
+
+            PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Json",
+					"JsonUtilities",
+					"NiagaraCore",
+					"Niagara",
+					"NiagaraShader",
+					"NavigationSystem",
+					// ... add private dependencies that you statically link with here ...
+                }
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+
+            PublicIncludePathModuleNames.AddRange(new string[] { "Launch" });
+
+            LoadLeapLib(Target);
+		}
+
+		public string GetUProjectPath()
+		{
+			return Path.Combine(ModuleDirectory, "../../../..");
+		}
+
+		private int HashFile(string FilePath)
+		{
+			string DLLString = File.ReadAllText(FilePath);
+			return DLLString.GetHashCode() + DLLString.Length;	//ensure both hash and file lengths match
+		}
+
+        private void CopyToBinaries(string Filepath)
+        {
+            string binariesDir = Path.Combine(BinariesPath, Target.Platform.ToString());
+            string filename = Path.GetFileName(Filepath);
+
+            if (!Directory.Exists(binariesDir))
+                Directory.CreateDirectory(binariesDir);
+
+            if (!File.Exists(Path.Combine(binariesDir, filename)))
+                File.Copy(Filepath, Path.Combine(binariesDir, filename), true);
+        }
+
+        public bool LoadLeapLib(ReadOnlyTargetRules Target)
+		{
+			bool IsLibrarySupported = false;
+
+			if (Target.Platform == UnrealTargetPlatform.Win64)
+			{
+				IsLibrarySupported = true;
+
+				string PlatformString = Target.Platform.ToString();
+
+				string ThirdPartyDllPath = Path.Combine(LibraryPath, PlatformString, "LeapC.dll");
+				string ThirdPartyDllManifPath = Path.Combine(LibraryPath, PlatformString, "LeapC.dll.manifest");
+                string BinDLLPath = Path.Combine(BinariesPath, PlatformString, "LeapC.dll");
+				string BinDLLManifPath = Path.Combine(BinariesPath, PlatformString, "LeapC.dll.manifest");
+                //Lib
+                PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, PlatformString, "LeapC.lib"));
+                //System.Console.WriteLine("plugin using lib at " + Path.Combine(LibraryPath, PlatformString, "LeapC.lib"));
+                // Copy third party DLLs to the BinariesPath 
+                RuntimeDependencies.Add(BinDLLPath, ThirdPartyDllPath);
+                RuntimeDependencies.Add(BinDLLManifPath, ThirdPartyDllManifPath);
+
+                // This will copy dlls if not copied already
+                CopyToBinaries(ThirdPartyDllPath);
+                CopyToBinaries(ThirdPartyDllManifPath);
+
+                if (IsEnginePlugin())
+				{
+					PublicDelayLoadDLLs.Add("LeapC.dll");
+                }
+				//Engine plugin, just add the dependency path
+				else
+				{
+					//DLL
+					System.Console.WriteLine("Project plugin detected, using dll at " + BinDLLPath);
+
+                    if (!Target.bBuildEditor)
+					{
+						PublicDelayLoadDLLs.Add("LeapC.dll");
+					}
+				}
+			}
+			else if (Target.Platform == UnrealTargetPlatform.Mac)
+			{
+				IsLibrarySupported = true;	
+                Setlib("Mac", "libLeapC.dylib");
+                Setlib("Mac", "libLeapC.5.dylib");
+            }
+			else if (Target.Platform == UnrealTargetPlatform.Android)
+			{
+				IsLibrarySupported = true;
+
+				string PlatformString = "Android";
+
+				PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, PlatformString, "arm64-v8a", "libLeapC.so"));
+
+				AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModulePath, "UltraleapTracking_APL.xml"));
+
+                PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
+            }
+			else if (Target.Platform == UnrealTargetPlatform.Linux)
+			{
+				IsLibrarySupported = true;
+
+				string PlatformString = "Linux";
+
+				PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, PlatformString, "x64", "libLeapC.so"));
+			}
+
+			return IsLibrarySupported;
+		}
+	}
+}
